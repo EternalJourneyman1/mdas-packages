0ac88921c2883d651108e6edbf53de57907ef94e
Demetrious Robinson <demetriousr@demetriousJMD6R.vmware.com>
2022-12-12 08:12:04 -0800

    Add postgres-for-kubernetes to lfs

--- packages/rabbitmq-in-docker/profile.d/post-setup.sh
+++
@@ -1,2 +0,0 @@
-set -x
-set -eo pipefail
--- packages/rabbitmq-in-docker/setup.d/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-set -eo pipefail
-
-helm repo add my-repo https://charts.bitnami.com/bitnami
-helm install my-release my-repo/postgresql
